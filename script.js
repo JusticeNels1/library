bc3a573fc8bf9b6b57e8050de9d4d7e8e887bc80
JusticeNels1 <jnelsondev@gmail.com>
2021-12-19 21:23:46 -0500

    Change to class constructor

--- script.js
+++ script.js
@@ -19,13 +19,15 @@ let myLibrary = [
                 // }
 ];
 
-function Book(title,author,pages,read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-};
+class Book {
 
+    constructor(title,author,pages,read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    };
+};
 
 // const deleteBtn = document.getElementById("delete")
 const submit = document.getElementById("submit")
